db6a4a86a725878ef7c521e3f7aa2a937bdcedcf
shuyuwx <158557746+shuyuwx@users.noreply.github.com>
2024-08-02 17:32:32 +0800

    Update header.php

--- usr/themes/icefox/components/header.php
+++ usr/themes/icefox/components/header.php
@@ -35,6 +35,13 @@ if (!defined('__TYPECHO_ROOT_DIR__'))
     <script type="text/javascript"
         src="<?php $this->options->themeUrl('assets/js/jquery.ui.touch.punch.min.js'); ?>"></script>
     <script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/axios.min.js'); ?>"></script>
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ngxg0tcnmu");
+    </script>
     <script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/viewer.js'); ?>"></script>
     <script type="text/javascript" src="<?php $this->options->themeUrl('assets/js/scrollload.min.js'); ?>"></script>
     <script defer src="<?php $this->options->themeUrl('assets/js/alpine.3.13.3.min.js'); ?>"></script>
